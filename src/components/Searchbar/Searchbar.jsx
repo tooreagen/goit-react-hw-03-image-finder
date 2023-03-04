0a11716a5411892dd1ddc26c5e59f6df46c2a3cd
tooreagen <tooreagen@gmail.com>
2023-03-04 18:10:07 +0200

    start

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,21 @@
+// import { Component } from 'react';
+// import { List } from './ContactsList.styled';
+// import PropTypes from 'prop-types';
+
+// export class ContactsList extends Component {
+//   render() {
+//     return {};
+//   }
+// }
+
+// ContactsList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ),
+//   filterString: PropTypes.string,
+//   onDelete: PropTypes.func.isRequired,
+// };
